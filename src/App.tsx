fb031b37d27fb0da00dd782c3d7096e73f06ccbe
wangtao <1182792703@qq.com>
2021-11-02 22:09:19 +0800

    完成页面的基本布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,61 @@
-function App() {
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./components/Nav"
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+export default function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from='/' to="/tags"/>
+            
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+     
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>Home</h2>;
+}
+
+function Tags() {
+  return <h2>About</h2>;
+}
+
+function Money() {
+  return <h2>Users</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>
+}
